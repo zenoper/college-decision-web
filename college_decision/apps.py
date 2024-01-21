8dd481aa99ebbcd5d942b4fd1ee6e3e30f70a878
Parzivald <bez@Muhammadqodirs-Mac.local>
2024-01-21 09:25:01 +0500

    landing page

---
+++ college_decision/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CollegeDecisionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'college_decision'
--- collegedecisionweb/settings.py
+++ collegedecisionweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "college_decision",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'collegedecisionweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
