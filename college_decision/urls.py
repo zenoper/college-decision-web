5135c8261cc9b39471e5a3c3d639b75312dacbe9
Parzivald <bez@Muhammadqodirs-Mac.local>
2024-01-22 12:01:48 +0500

    send letter form

--- college_decision/urls.py
+++ college_decision/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, pricing, letter, submitted_info
 
 urlpatterns = [
     path('', home, name='home'),
+    path('pricing/', pricing, name='pricing'),
+    path('send_letter/', letter, name='send_letter'),
+    path('send_letter/submit', submitted_info, name="submitted_info")
 ]
--- college_decision/views.py
+++ college_decision/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'index.html')
+
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+
+def letter(request):
+    return render(request, 'send_letter.html')
+
+
+def submitted_info(request):
+    if request.method == 'POST':
+        full_name = request.POST.get('full_name')
+        email = request.POST.get('email')
+        university = request.POST.get('university')
+        decision = request.POST.get('decision')
+
+        print(full_name, email, university, decision)
+
+        return HttpResponse(f'Thank you, {full_name}! Your submission has been received.')
+
+    return render(request, 'send_letter.html')
