ed10e329b3a61c1f1ed81f954f5242e218a21a37
Parzivald <bez@Muhammadqodirs-Mac.local>
2024-02-07 09:00:06 +0500

    deploy

--- college_decision/urls.py
+++ college_decision/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('pricing/', views.pricing, name='pricing'),
+    # path('pricing/', views.pricing, name='pricing'),
     path('send_letter/', views.letter, name='send_letter'),
     path('send_letter/submit', views.submitted_info, name="submitted_info"),
     path('peasant/', views.peasant, name='peasant'),
-    path('webhook/', views.stripe_webhook, name='stripe_webhook'),
 ]
