20ff109a1884bcfbdf5577cb4e427865db785e83
Parzivald <bez@Muhammadqodirs-Mac.local>
2024-02-07 09:34:07 +0500

    deploy 3.1

--- college_decision/views.py
+++ college_decision/views.py
@@ -4,12 +4,7 @@ from .send_email import send_email
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-import stripe
 import json
-
-stripe.api_key = "sk_test_51OcOkAH7wZCCmn5aCIr3JMdMXXThlfWdA6rtWqp5KhrrE5771hVSnP11B0eMpBYh89ghWd6JaIMy81BONTCB69K200j9W7Y3zT"
-
-endpoint_secret = 'whsec_ee8fcf7616f27578757c8e44f4e02bf39e1ea96e0b87a3a7d92838776a326e45'
 
 
 def home(request):
