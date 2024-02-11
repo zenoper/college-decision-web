8eac38846f07135c87a5bd0055d968260decfa68
Parzivald <bez@Muhammadqodirs-Mac.local>
2024-02-11 12:13:37 +0500

    ads request

--- collegedecisionweb/settings.py
+++ collegedecisionweb/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.college-decision.com', 'college-decision.com', "127.0.0.1"]
 
